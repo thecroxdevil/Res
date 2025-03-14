f8d0134379bbe99c26d70af7768c4f7504c40e99
thecroxdevil <55011065+thecroxdevil@users.noreply.github.com>
2025-03-14 13:56:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-# Try accessing a custom secret
-custom_value = st.secrets.get("HF_TOKEN", None)
-
-if custom_value:
-    st.write(f"Custom Secret Value: {custom_value}")
-else:
-    st.error("Custom Secret Value not found!")
+st.write(st.secrets)  # This should print the stored secrets (but not expose them publicly)
